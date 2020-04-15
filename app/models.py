263035897082b566a6896c83505c09693c292769
machnine <mianchen@gmail.com>
2020-04-15 17:03:43 +0100

    Chapter 8.3

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from flask_login import UserMixin
 from app import db, login
 from hashlib import md5
 
+
+followers = db.Table('followers', db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                                  db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -12,7 +16,12 @@ class User(UserMixin, db.Model):
     posts  = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
+    followed = db.relationship('User', secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'), 
+                               lazy='dynamic')
     def __repr__(self):
         return f'<User {self.username}>'
         
@@ -26,6 +35,18 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=monsterid&s={size}"
         
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed(append(user))
+    
+    def unfollow(self,user):
+        if self.is_following(user):
+            seld.followed.remove(user)
+            
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+        
+        
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -36,6 +57,8 @@ class Post(db.Model):
         return f'<Post {self.body}>'
         
 
+       
+
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))    
+    return User.query.get(int(id))     
